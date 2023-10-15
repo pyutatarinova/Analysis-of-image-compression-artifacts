f9aa5f9f12ebc438962902d1e4d68c0bd449f7bd
Tatarinova Polina <polandddddya@gmail.com>
2023-10-15 16:53:26 +0300

    import libraries, find and create folders with images

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import cv2
+import subprocess
+
+
+print('Введите название папки с исходными изображениями:')
+original_folder = input()  # папка с исходными изображениями
+while not os.path.exists(original_folder):
+    print('Такой папки не существует. Введите корректное название:')
+    original_folder = input()
+compressed_folder = "compressed_images"  # папка со сжатыми изображениями
+if not os.path.exists(compressed_folder):
+    os.mkdir(compressed_folder)
+diff_folder = "diff_images"  # папка с разницей изображений
+if not os.path.exists(diff_folder):
+    os.mkdir(diff_folder)
+result_file = "results.txt"  # файл для записи результатов анализа
